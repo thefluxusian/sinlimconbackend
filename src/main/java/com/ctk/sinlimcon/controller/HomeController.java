df0efd842a30afcd1f5201177c2aceed6ff49d9d
ctk03272 <ctk0327@naver.com>
2018-01-16 22:50:42 +0900

    test main page

--- src/main/java/com/ctk/sinlimcon/controller/HomeController.java
+++ src/main/java/com/ctk/sinlimcon/controller/HomeController.java
@@ -1,10 +1,17 @@
 package com.ctk.sinlimcon.controller;
 
 
+import java.util.HashMap;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import com.ctk.sinlimcon.service.DesignService;
 
 
@@ -14,10 +21,11 @@ import com.ctk.sinlimcon.service.DesignService;
 @Controller
 public class HomeController {
 
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@ResponseBody
+	public HashMap<String, Object> HomePage() {
 
-	@Autowired
-	private DesignService designService;
-
+		return null;
+	}
 
 }
